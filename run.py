3fbca2283f5e003a4fadc8cf052bb6370e05d195
Daniel Stauffer <info@rebellion-webdesign.com>
2023-08-10 16:04:20 +0200

    Add function greeter_input()

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 import time ## Time is needed to to have delays for the greeter messages
 #import requests ## Requests will handle the http-GET part of the app
 
+
+
 def greeter():
     print("Hi and welcome to Precious Password!")
     time.sleep(5)
@@ -12,7 +14,21 @@ def greeter():
     print("- Check if your password is in the PWNED PASSWORD database")
     time.sleep(2)
     print("- Check if your password was hacked previously")
-    time.sleep(5)
-    greeter_input = input("Do you want to enter a password now (y/n)?\n")
+    time.sleep(2)
 
 greeter()
+
+def greeter_input(*user_response):
+    user_response = input("Do you want to enter a password now (y/n)?\n")
+
+    if user_response == "y":
+        print("Works!")
+    elif user_response == "n":
+        print("Have anice day!")
+        exit()
+    else:
+        print("Please input y or n")
+        greeter_input()
+
+greeter_input()
+
