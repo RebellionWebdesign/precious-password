62ba34a6085fbd5f1b72047ad7c987711993bae5
Daniel Stauffer <info@rebellion-webdesign.com>
2023-08-11 09:25:40 +0200

    Create main class PasswordCheck()

--- run.py
+++ run.py
@@ -2,6 +2,14 @@
 import time ## Time is needed to to have delays for the greeter messages
 #import requests ## Requests will handle the http-GET part of the app
 
+class PasswordCheck():
+    def __init__(self, pw_clean, pw_hash, pw_prefix, pw_strength, pw_in_list):
+        self.pw_clean = pw_clean
+        self.pw_hash = pw_hash
+        self.pw_prefix = pw_prefix
+        self.pw_strength = pw_strength
+        self.pw_in_list = pw_in_list
+
 def greeter():
     print("Hi and welcome to Precious Password!")
     time.sleep(5)
