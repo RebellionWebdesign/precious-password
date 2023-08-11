f10b06e2a3fd66a650adb16f1513ec032706817f
Daniel Stauffer <info@rebellion-webdesign.com>
2023-08-11 11:30:19 +0200

    Add hashlib import and password_input()

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 ## Here we need to import some modules we use
 import time ## Time is needed to to have delays for the greeter messages
+import hashlib ## This will proide the hash algorithm (SHA-1) for the pwned password api
 #import requests ## Requests will handle the http-GET part of the app
 
 class PasswordCheck():
@@ -22,11 +23,22 @@ def greeter():
     print("- Check if your password is commonly used")
     time.sleep(2)
 
+def password_input(user_password):
+    user_password = input("Please choose a password to test: \n")
+
+    if user_password == "":
+        print("Sorry, this is not a password. Try again.")
+        password_input()
+    else:
+        check = PasswordCheck(user_password.upper(), None, None, None, None)
+        print(check.pw_clean)
+
 def greeter_input():
     user_response = input("Do you want to enter a password now (y/n)?\n")
 
     if user_response == "y":
         print("Okay, let´s go!")
+        password_input()
     elif user_response == "n":
         print("Have a nice day!")
         exit()
