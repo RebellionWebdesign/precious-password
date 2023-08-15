d950595e79f876b607f5e6c180769e68547113a1
Daniel Stauffer <info@rebellion-webdesign.com>
2023-08-15 11:47:03 +0200

    Add password hash func and attribute

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import pyfiglet
 from simple_term_menu import TerminalMenu
 import hashlib
+import requests
 
 #Converts the passwords.txt file to a list
 common_passwords = open("passwords.txt", "r")
@@ -10,16 +11,17 @@ common_passwords.close()
 
 # Class holding the password data
 class PasswordCheck:
-    def __init__(self, pw_clean, pw_hash, pw_prefix, pw_strength,
-                 pw_in_list):
+    def __init__(self, pw_clean, pw_hash, pw_prefix, pw_in_db,
+                 pw_in_list, pw_suffix):
         self.pw_clean = pw_clean
         self.pw_hash = pw_hash
         self.pw_prefix = pw_prefix
-        self.pw_strength = pw_strength
+        self.pw_in_db = pw_in_db
         self.pw_in_list = pw_in_list
+        self.pw_suffix = pw_suffix
 
 #Instance of the class holding password data
-pwc_instance = PasswordCheck("", "", "", "", "")
+pwc_instance = PasswordCheck("", "", "", "", "", "")
 
 def get_password():
     user_password = input("Please enter the password you need to check: \n")
@@ -40,9 +42,19 @@ def check_password_frequency():
         print("Password is not often used. Nice!")
         pwc_instance.pw_in_list = "NO"
 
+def hash_password():
+    if pwc_instance.pw_clean == "":
+        print("Sorry, no password wa given")
+        get_password()
+    else:
+        pwc_instance.pw_hash = hashlib.sha1(bytes(pwc_instance.pw_clean, "utf-8")).hexdigest()
+        pwc_instance.pw_prefix = pwc_instance.pw_hash[0:5]
+        print(pwc_instance.pw_prefix)
+
 def main():
     get_password()
     check_password_frequency()
+    hash_password()
 
 if __name__ == "__main__":
     main()
